1262babee610a36c347c3e4dd9b9142935edc908
DimitriyAB <dimkagithub3453@mail.ru>
2023-11-06 14:42:28 +0500

    Lab 11-2

--- BaranovGlazki/BaranovGlazki/ProductPage.xaml.cs
+++ BaranovGlazki/BaranovGlazki/ProductPage.xaml.cs
@@ -27,11 +27,100 @@ namespace BaranovGlazki
             var currentServices = Baranov_glazkiEntities.GetContext().Agent.ToList();
 
             ServiceListView.ItemsSource = currentServices;
+
+            ComboTypeSort.SelectedIndex = 0;
+            ComboTypeAgTy.SelectedIndex = 0;
+
+            UpdateProduct();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateProduct();
+        }              
+        private void ComboTypeAgTy_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void ComboTypeSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProduct() ;
+        }
+
+        private void UpdateProduct()
+        {
+            var currentProduct = Baranov_glazkiEntities.GetContext().Agent.ToList();
+
+
+            if (ComboTypeAgTy.SelectedIndex == 0)
+            {
+                currentProduct = currentProduct.ToList();
+            }
+            if (ComboTypeAgTy.SelectedIndex == 1)
+            {
+                currentProduct = currentProduct.Where(p => (p.AgentTypeID == 3)).ToList();
+            }
+            if (ComboTypeAgTy.SelectedIndex == 2)
+            {
+                currentProduct = currentProduct.Where(p => (p.AgentTypeID == 5)).ToList();
+            }
+            if (ComboTypeAgTy.SelectedIndex == 3)
+            {
+                currentProduct = currentProduct.Where(p => (p.AgentTypeID == 1)).ToList();
+            }
+            if (ComboTypeAgTy.SelectedIndex == 4)
+            {
+                currentProduct = currentProduct.Where(p => (p.AgentTypeID == 2)).ToList();
+            }
+            if (ComboTypeAgTy.SelectedIndex == 5)
+            {
+                currentProduct = currentProduct.Where(p => (p.AgentTypeID == 4)).ToList();
+            }
+            if (ComboTypeAgTy.SelectedIndex == 6)
+            {
+                currentProduct = currentProduct.Where(p => (p.AgentTypeID == 6)).ToList();
+            }
+
+            currentProduct = currentProduct.Where(p => p.Title.ToLower().Contains(TBoxSearch.Text.ToLower())
+            ||p.Phone.Replace("+","").Replace(" ","").Replace("(","").Replace(")","").Replace("-","").Contains(TBoxSearch.Text)
+            ||p.Email.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            if (ComboTypeSort.SelectedIndex == 0)
+            {
+                currentProduct = currentProduct.ToList();
+            }
+            if (ComboTypeSort.SelectedIndex == 1)
+            { 
+                currentProduct = currentProduct.OrderBy(p => p.Title).ToList();
+            }
+            if (ComboTypeSort.SelectedIndex == 2)
+            {
+                currentProduct = currentProduct.OrderByDescending(p => p.Title).ToList();
+            }
+            if (ComboTypeSort.SelectedIndex == 3)
+            {
+                
+            }
+            if (ComboTypeSort.SelectedIndex == 4)
+            {
+
+            }
+            if (ComboTypeSort.SelectedIndex == 5)
+            {
+                currentProduct = currentProduct.OrderBy(p => p.Priority).ToList();
+            }
+            if (ComboTypeSort.SelectedIndex == 6)
+            {
+                currentProduct = currentProduct.OrderByDescending(p => p.Priority).ToList();
+            }
+
+            ServiceListView.ItemsSource = currentProduct;
         }
     }
 }
